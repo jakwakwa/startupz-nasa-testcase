0fa83a651d785aefc5e086fbc16aabae22a38c0e
Jaco Kotzee <jakwakwa@gmail.com>
2023-02-12 16:40:29 +0200

    installed chakra ui lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
 // import reactLogo from './assets/react.svg'
 import "./App.css";
+import { ChakraProvider } from "@chakra-ui/react";
 import Search from "./pages/Search/Search";
 
 function App() {
   const [count, setCount] = useState(1);
 
   return (
-    <div className="App">
-      <div></div>
-      <h1>NASA MEDIA LIBRARY </h1>
-      <h2>Search for images on NASA's images' API</h2>
-      <Search />
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <div></div>
+        <h1>NASA MEDIA LIBRARY </h1>
+        <h2>Search for images on NASA's images' API</h2>
+        <Search />
+      </div>
+    </ChakraProvider>
   );
 }
 
